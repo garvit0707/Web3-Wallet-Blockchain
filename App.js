a2e6ab14719c5ee04c7525a7ad8d000047475f38
Garvit Sharma <161028881+garvit0707@users.noreply.github.com>
2026-03-26 00:35:30 +0530

    root change app

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { store } from './src/store';
+import AppNavigator from './src/navigation/AppNavigator';
+
+export default function App() {
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Provider store={store}>
+        <AppNavigator />
+      </Provider>
+    </GestureHandlerRootView>
+  );
+}
